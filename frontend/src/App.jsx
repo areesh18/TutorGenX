0793b59e41b36e27ab6b3f445018b3d41c70957f
Areesh18 <areeshzafar396@gmail.com>
2025-08-24 03:39:23 +0530

    removed Navbar.jsx except in landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,37 +16,15 @@ function AppWrapper() {
 }
 
 function App() {
-  const location = useLocation();
-
-  const getBgColor = () => {
-    switch (location.pathname) {
-      /* case "/":
-        return "bg-transparent";
-      case "/signup":
-        return "bg-green-500";
-      case "/login":
-        return "bg-purple-500";
-      case "/dashboard":
-        return "bg-yellow-500"; */
-      default:
-        return "bg-gray-800";
-    }
-  };
-  const showNavbar = !(
-    location.pathname === "/" ||
-    location.pathname === "/login" ||
-    location.pathname === "/signup"
-  );
   return (
     <>
-      {showNavbar && <Navbar bgClass={getBgColor()} />}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         {/* <Route element={<Layout/>}> */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Landing />} />
-          <Route path="/learn/:roadmapId" element={<LearnPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/learn/:roadmapId" element={<LearnPage />} />
         {/* </Route> */}
       </Routes>
     </>
